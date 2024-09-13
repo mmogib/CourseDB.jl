e0ebf40720dfb93ba3061a18fa5c229fb867317c
Documenter.jl <documenter@juliadocs.github.io>
2024-09-13 01:11:40 +0000

    build based on cd57da0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KFUPMCourseDB","category":"page"},{"location":"#KFUPMCourseDB","page":"Home","title":"KFUPMCourseDB","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KFUPMCourseDB.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KFUPMCourseDB]","category":"page"}]
+}
